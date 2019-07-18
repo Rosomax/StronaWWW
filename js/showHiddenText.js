77f9f75ee55f6f8294c49f1263da39b0a86e11d2
Rosomax <m.biaek91@gmail.com>
2019-07-19 01:38:07 +0200

    Pierwszy commit

---
+++ js/showHiddenText.js
@@ -0,0 +1,6 @@
+ function hs() 
+    {
+        x=document.getElementsByClassName("articleContentGit")[0];
+        if(x.style.display == "none") x.style.display = "block";
+        else x.style.display = "none"
+     }
