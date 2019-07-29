21cbed50356828e4b9bfd66f1db5de9fd1354532
Rosomax <m.biaek91@gmail.com>
2019-07-30 00:56:15 +0200

    Dodano zapytanie o media dla index, poprawiono content, poprawiono semantyke html

--- js/jqueryscripts.js
+++ js/jqueryscripts.js
@@ -1,29 +1,43 @@
-$(document).ready(function()
-{
-    $("div.boxMenuLeft").click(function()
-    {
-        $(this).animate({ width: "0%", height: "0px", padding: "0", 'font-size': "0px",
-         "margin-right":"18%"}, 1500)
-        setTimeout(function(){
+$(document).ready(function () {
+    $("div.boxMenuLeft").click(function () {
+        $(this).animate({
+            width: "0%",
+            height: "0px",
+            padding: "0",
+            'font-size': "0px",
+            "margin-right": "18%",
+
+
+        }, 1500)
+        setTimeout(function () {
             $(location).attr('href', 'csharp.html')
         }, 1150)
     });
 
-    $(".boxMenu").click(function()
-    {
-            $(this).animate({ width: "0%", height: "0px", padding: "0", 'font-size': "0px",
-             "margin-right":"18%"}, 1500)
-            setTimeout(function(){
-                $(location).attr('href', 'git.html')
-            }, 1010)
+    $(".boxMenu").click(function () {
+        $(this).animate({
+            width: "0%",
+            height: "0px",
+            padding: "0",
+            'font-size': "0px",
+            "margin-right": "18%"
+        }, 1500)
+        setTimeout(function () {
+            $(location).attr('href', 'git.html')
+        }, 1010)
     });
 
-    $(".boxMenuRight").click(function()
-    {
-            $(this).animate({ width: "0%", height: "0px", padding: "0", 'font-size': "0px",
-             "margin-right":"18%", "margin-bottom":"14.14%"}, 1500)
-            setTimeout(function(){
-                $(location).attr('href', 'js.html')
-            }, 1010)
+    $(".boxMenuRight").click(function () {
+        $(this).animate({
+            width: "0%",
+            height: "0px",
+            padding: "0",
+            'font-size': "0px",
+            "margin-right": "18%",
+            "margin-bottom": "14.30%"
+        }, 1500)
+        setTimeout(function () {
+            $(location).attr('href', 'js.html')
+        }, 1010)
     });
 });
